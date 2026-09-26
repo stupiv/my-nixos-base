f22742ae418205d2700253009687673bb17078bb
stupiv <stupiv@outlook.com>
2026-09-26 08:13:58 +0700

    Print timestamps and duration

--- nixos-base/terminal/fish.nix
+++ nixos-base/terminal/fish.nix
@@ -4,6 +4,13 @@
     enable = true;
     interactiveShellInit = ''
       set fish_greeting (uuidgen -r && date)
+
+      function stamp_start --on-event fish_preexec
+        set -g __cmd_started (date '+%T')
+      end
+      function stamp_end --on-event fish_postexec
+        printf '[%s → %s] took %ss\n' $__cmd_started (date '+%T') (math --scale=2 "$CMD_DURATION / 1000")
+      end
     '';
   };
 }
